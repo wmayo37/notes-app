7bd2185961f30254b9824efe361d0652d6c9affb
Will Mayo <wm128@duke.edu>
2025-04-18 03:30:08 -0400

    tried to fix width

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,40 @@
+import { useState } from "react";
 import "./App.css";
 
+type Note = {
+  id: number;
+  title: string;
+  content: string;
+};
+
 const App = () => {
+  const [notes, setNotes] = useState<Note[]>([
+    {
+      id: 1,
+      title: "note title 1",
+      content: "content 1",
+    },
+    {
+      id: 2,
+      title: "note title 2",
+      content: "content 2",
+    },
+    {
+      id: 3,
+      title: "note title 3",
+      content: "content 3",
+    },
+    {
+      id: 4,
+      title: "note title 4",
+      content: "content 4",
+    },
+    {
+      id: 5,
+      title: "note title 5",
+      content: "content 5",
+    },
+  ]);
   return (
     <div className="app-container">
       <form className="note-form">
@@ -9,13 +43,15 @@ const App = () => {
         <button type="submit">Add Note</button>
       </form>
       <div className="notes-grid">
-        <div className="note-item">
-          <div className="notes-header">
-            <button>x</button>
+        {notes.map((note) => (
+          <div className="note-item">
+            <div className="notes-header">
+              <button>x</button>
+            </div>
+            <h2>{note.title}</h2>
+            <p>{note.content}</p>
           </div>
-          <h2>Note Title</h2>
-          <p>Note Content</p>
-        </div>
+        ))}
       </div>
     </div>
   );
